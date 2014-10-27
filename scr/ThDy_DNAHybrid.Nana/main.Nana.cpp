a337bddba02bc112a68b62085f36bf3648ad324d
qPCR4vir <arielvina@yahoo.es>
2014-10-27 21:24:09 +0100

    implemented exit
    Former-commit-id: 86a53502f390cfe3014c0597c3e26c429fdb0e4b

--- scr/ThDy_DNAHybrid.Nana/main.Nana.cpp
+++ scr/ThDy_DNAHybrid.Nana/main.Nana.cpp
@@ -70,6 +70,227 @@ int main(int argc, char *argv[])
     //std::cout.rdbuf(coutbuf); //reset to standard output again
 
 } 
+void  SetupPage::AsignWidgetToFields ()  
+    {
+      _setup<< link( _Pr._cp._OutputFile      ,       _results  )
+            << link( _Pr._cp._InputTargetFile ,       _targets  )
+            << link( _Pr._cp._TRecurDir         ,     _chkTargRecDir)
+            << link( _Pr._cp._TDirStrOnly      ,     _chkTargOnlyStruct)
+            << link( _Pr._cp._NonTargetFile     ,       _nTsec  )
+            << link( _Pr._cp._nTRecurDir      ,     _chk_nTgRecDir)
+            << link( _Pr._cp._nTDirStrOnly      ,     _chk_nTgOnlyStruct)
+            << link( _Pr._cp._PCRfiltrPrFile  ,       _PCRfiltre)
+            << link( _Pr._mPCR._InputSondeFile , _PrimersFilePCR)            
+            << link( _Pr._mPCR._PrRecurDir      ,     _chkPrimRecDir)
+            << link( _Pr._mPCR._PrDirStrOnly      ,     _chkPrOnlyStruct)
+            << link( _Pr._uArr._InputSondeFile , _Prob_uArr)            
+            << link( _Pr._uArr._PrRecurDir      ,     _chkProbRecDir)
+            << link( _Pr._uArr._PrDirStrOnly      ,     _chkProbOnlyStruct)
+            << link( _Pr._cp._InputNNFile       , _NNParamFile  )
+            << link( _Pr._cp.ConcSd	    ,       numUpDowSdConc  )
+            << link( _Pr._cp.ConcSalt	     , numUpDowSalConc  )
+            << link( _Pr._cp.ConcTg	    ,       numUpDowTgConc  )
+            << link( _Pr._cp.Ta	            ,       numUpDowTa  )        
+            << link( _Pr._cp.SaltCorr	  ,      comBoxSalMeth  )        
+            << link( _Pr._cp.TAMeth       ,       comBoxTAMeth  )        
+            << link( _Pr._cp.st_savTm	  ,       ckBx_savTm    )        
+            << link( _Pr._cp.st_savPos	  ,       ckBx_savPos   )        
+            << link( _Pr._cp.st_savG 	  ,         ckBx_savG   )        
+            << link( _Pr._cp.st_savAlign  ,     ckBx_savAlign   )        
+            << link( _Pr._cp.st_savProj   ,     ckBx_savProj    )        
+            << link( _Pr._cp.st_savG_Plasm , ckBx_savG_Plasm    )        
+            << link( _Pr._cp.st_savTm_Plasm , ckBx_savTm_Plasm  )     
+            << link( _Pr._cp.st_savLog 	  ,       ckBx_savLog   )        
+            << link( _Pr._cp.st_Exp_sond  ,ckBx_savExportSond   )        
+            << link( _Pr._cp.st_ExpTarg	 ,ckBx_savExportTarg    )        
+            << link( _Pr._cp.saveNNPar     , ckBx_savNNParam    )        
+            << link( _Pr._cp.loadNNPar    , ckBx_loadNNParam    )        
+          ;
+            
+        _place.field("Project"  )    <<  _proj        ;
+	    _place.field("Results" )     <<  _results   ;
+        _place.field("_targets" )    <<  _targets ;
+        _place.field("TargOpt"  )    << _chkTargRecDir  << _chkTargOnlyStruct ;
+        _place.field("_nTsec"   )    <<  _nTsec   ;
+        _place.field("nTargOpt" )    << _chk_nTgRecDir <<  _chk_nTgOnlyStruct          ;
+        _place.field("_PCRfiltre")    <<  _PCRfiltre   ;
+        _place.field("_PrimersFilePCR" )    <<  _PrimersFilePCR ;
+        _place.field("_PrimersFilePCROpt"  )    << _chkPrimRecDir  << _chkPrOnlyStruct ;
+        _place.field("_Prob_uArr" )    <<  _Prob_uArr ;
+        _place.field("_Prob_uArrOpt"  )    << _chkProbRecDir  << _chkProbOnlyStruct ;
+	    _place.field("NN_param" )    << _NNParamFile  ;
+	    _place.field("buttons"  )    <<  _set_def_proj << _load_def_proj;
+	    _place.field("checks"   )    << "save result" << ckBx_savTm    << ckBx_savPos     <<ckBx_savG         << ckBx_savAlign 
+                                                      << ckBx_savProj  << ckBx_savG_Plasm << ckBx_savTm_Plasm << ckBx_savLog
+                                                      << ckBx_savExportSond << ckBx_savExportTarg<< ckBx_savNNParam<< ckBx_loadNNParam;
+
+	    _place.field("ConcST"  )        << numUpDowSdConc   
+                                        << numUpDowTgConc ;
+	    _place.field("ConcSaltTa"  )    << numUpDowSalConc 
+                                        << numUpDowTa ;
+	    _place.field("SMeth"  )         << " Salt Correct. Method:"	   <<  comBoxSalMeth;
+	    _place.field("AMeth"  )         << " ThDy Align. Method"       <<  comBoxTAMeth ;
+    }
+void  SetupPage::MakeResponive()
+    {
+        _proj.add_filter(STR("ThDy project"),STR("*.ThDy.txt"));
+        _proj.onOpenAndSelectFile ([this](const nana::string &file)
+        { 
+            this->LoadProject (  file  );
+                std::wcerr << L"onOpenAndSelectFile: Loaded Project file: " << file << std::endl;
+        } );
+        _proj.onSaveFile          ([this](const nana::string &file)
+        { 
+            this->_Pr.save (nana::charset ( (  file  ))); 
+                std::wcerr << L"onSaveFile: Saved Project file: " << file << std::endl;
+        } );
+
+        AddFastaFiltre(_targets );
+        AddFastaFiltre(_nTsec );
+        AddFastaFiltre(_PCRfiltre );
+        AddFastaFiltre(_PrimersFilePCR );
+
+        _NNParamFile.add_filter      (STR("Nearest Neibrhud Parametr"),STR("*.NN.csv"));
+        _NNParamFile.onOpenAndSelect ([this]()
+            { 
+
+            //assert((  std::cerr<< "\nBefore loading NNfile, SetupPage: " , true  ));;
+            //assert((  std::wcerr<< caption() << std::endl , true  ));;
+            
+            std::ifstream nn(_Pr._cp._InputNNFile.get());
+                _Pr._cp._pSaltCorrNNp->LoadNNParam(nn ) ;
+
+            //assert((  std::cerr << "onOpenAndSelect: Opened NNp file: " << _Pr._cp._InputNNFile.get() << std::endl, true  ));;
+                return true;
+
+            } );
+        _NNParamFile.onSave          ([this]()
+        {
+
+            //assert((  std::cerr<< "\nBefore saving NNfile, SetupPage: " , true  ));;
+            //assert((  std::wcerr<< caption() << std::endl , true  ));;
+            
+            std::ofstream{ _Pr._cp._InputNNFile.get() /* + STR(".NN.csv") */} << *_Pr._cp._pSaltCorrNNp;        
+
+            //assert((  std::cerr << "onSave: Saved NNp file: " << _Pr._cp._InputNNFile.get() << std::endl, true  ));;
+         } );
+
+        _set_def_proj .events().click ([&](){ setAsDefProject() ;} );
+        _load_def_proj.events().click ([&](){ RestDefPr      () ;} );
+    }
+void  SetupPage::SaveProj()
+	{	 
+        if(  _proj.Canceled () )  return;
+        _Pr.save (nana::charset ( _proj.FileName())); 
+	}
+ //   void  OpenNN()
+	//{	 
+ //        if( ! _NNParamFile.Canceled () )   
+ //           return LoadProject ( nana::charset ( _NNParamFile.FileName() )) ;  
+	//}
+ //   void  SaveNN()
+	//{	 
+ //       if(  _NNParamFile.Canceled () )  return;
+ //       _Pr.save (nana::charset ( _NNParamFile.FileName())); 
+	//}
+void  SetupPage::setAsDefProject()
+    {
+		string caption = "Set current setting as Default project";
+		string message = std::string("This will overwrite the current Default Project." )        + "\n\n"
+						+  "Are you sure?"   + "\n\n"
+						+ "\tYes:  The default project will be overwrited. " + "\n"
+						+ "\tNo:  No action will be taken. " + "\n"
+						;
+		switch ( (nana::msgbox(  *this, nana::charset (caption) , nana::msgbox::yes_no )
+                        <<  message
+                    ).icon(nana::msgbox::icon_question ) .show (  ))
+		{
+			case  nana::msgbox::pick_yes :  
+                                    _Pr.save_asDefPr() ; 					 // crea el Def Project.
+				return;
+
+			case  nana::msgbox::pick_no:    
+            default:;
+        }
+    }
+void  SetupPage::RestDefPr	 ( )		// Restore (USE) Deff  Proj File
+{		 
+    try{
+		    _Pr.load_defPr() ;			// cuando no existe Def Project: 1ra vez que se usa el prog??
+		}
+	catch ( std::exception& e)
+	{ 
+		(nana::msgbox ( STR("Error loading Def Project" ) )<< e.what()).show() ;
+ 	}		 
+}
+void  SetupPage::AddMenuItems(nana::menu& menu)
+{
+//      		_menuFile.append  (STR("&Open..."   ),[this](nana::menu::item_proxy& ip){ this->_OSbx.open(nana::string(nana::charset(this->_textBox.filename())));this->OpenFile()  ;}                );
+//      _menuFile.append  (STR("&Save"      ),[&](nana::menu::item_proxy& ip){  ForceSave(nana::string(nana::charset(_textBox.filename())) ) ;}   );
+	//_menuFile.append  (STR("Save &As..."),[&](nana::menu::item_proxy& ip){ _OSbx.save(nana::string(nana::charset(_textBox.filename())));SaveFile() ;} );
+
+    menu.append(STR("New"    )  , [&](nana::menu::item_proxy& ip)  {  ;  } );
+    menu.append(STR("Open...")  , [&](nana::menu::item_proxy& ip)  
+    { 
+        _proj.open(_proj.FileName()); /*OpenProj() ;*/   
+        if (!_proj.Canceled())
+            LoadProject( nana::charset(_proj.FileName()));
+        //this->_OSbx.open(nana::string(nana::charset(this->_textBox.filename())));this->OpenFile()  ;}                );
+
+    } );
+    menu.append(STR("Save...")  , [&](nana::menu::item_proxy& ip)  
+    { 
+        _proj.save(_proj.FileName()); 
+        if( ! _proj.Canceled () )   
+            _Pr.save (nana::charset ( _proj.FileName())); 
+
+        //SaveProj() ;  
+    } );
+    menu.append_splitter();
+    menu.append(STR("Set as deffault") , [&](nana::menu::item_proxy& ip)  {;  });
+    menu.append(STR("Restore deffault"), [&](nana::menu::item_proxy& ip)  {;  });
+    menu.append_splitter();
+    menu.append(STR("Exit"    )  , [&](nana::menu::item_proxy& ip)  {  _Pr.close();  } );
+
+}
+void  SetupPage::LoadProjectAndReg(nana::string file)
+    {
+        LoadProject(file);
+ 		_proj.FileName(file  );
+   }
+void  SetupPage::LoadProject(nana::string file)
+	{
+		try
+		{
+			_Pr.load(  nana::charset (  file ));
+ 		}
+		catch (std::exception& e)
+		{
+			string caption = "Error trying to load the project file:";
+			string message = string (nana::charset (  file ))         + "\n\n"
+							+  e.what()   + "\n\n"
+							+  "Use the Default project?"   + "\n\n"
+							+ "\tYes:  The default project file will be loaded. " + "\n"
+							+ "\tNo:  Select a project file to be loaded. " + "\n"
+							+ "\tCancel: Use the values correctly loaded mixed with the\t\t\t previus existing. "
+							;
+			switch ( (nana::msgbox(  *this, nana::charset (caption) , nana::msgbox::yes_no_cancel )
+                            <<  message
+                        ).icon(nana::msgbox::icon_error) .show (  ))
+			{
+				case  nana::msgbox::pick_yes :  
+					    _Pr.load_defPr();
+                        _proj.FileName(nana::charset ( _Pr.ProjetFile ()  ));
+					return;
+
+				case  nana::msgbox::pick_no:    
+                        _proj.open (nana::charset (file));
+                        if ( !  _proj.Canceled() )
+                                LoadProject(nana::charset (  _proj.FileName()));
+                        return;
+			}
+		}
+	}
 
    SetupPage::SetupPage          (ThDyNanaForm& tdForm)
         : _Pr           (tdForm), 
